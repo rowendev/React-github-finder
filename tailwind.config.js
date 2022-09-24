4611b15329852b15f4a0689f95f7aeb505de0682
rowendev <rowendevtw@gmail.com>
2022-09-24 13:48:21 +0800

    add phone screen styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
-        xs: "350px",
+        "xs": "350px",
         ...defaultTheme.screens,
       },
     },
